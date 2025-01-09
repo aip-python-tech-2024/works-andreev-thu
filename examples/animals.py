68ea80936e94ff2524408cc162505e012cb2e38a
Nikolay Andreev <bakasaru@list.ru>
2025-01-09 17:34:09 +0300

    feat: add inheritance example with animals

--- examples/animals.py
+++ examples/animals.py
@@ -1,9 +1,24 @@
-class Dog:
+class Mammal:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Mammal(name={self.name})'
+
+
+class Dog(Mammal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
     def speak(self):
         print('Woof!')
 
+    def __repr__(self):
+        return f'Dog(name={self.name}, breed={self.breed})'
 
-class Cat:
+
+class Cat(Mammal):
     def speak(self):
         print('Meow!')
 
@@ -13,9 +28,15 @@ class Duck:
         print('Quack!')
 
 
-dog = Dog()
-cat = Cat()
+dog = Dog('Шарик', 'Той-терьер')
+cat = Cat('Феликс')
 duck = Duck()
+
+mammal = Mammal('Bobik')
+print(mammal)
+
+print(dog)
+print(cat)
 
 animals = [dog, cat, duck]
 for animal in animals:
